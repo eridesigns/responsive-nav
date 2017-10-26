7f7edae52df69052951da5159baa572e5ae054ec
Olivia Orr <oorr90@gmail.com>
2017-10-26 13:36:19 -0400

    Creating the show/hide menu

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
+//Grab the three line nav image
+    //add the showNav event listener
+    //set wasClicked to false, since it has not been clicked yet
 var navButton = document.getElementById("responsive-button");
 navButton.addEventListener("click", showNav);
+navButton.wasClicked = false;
 
+//Store the ul in a variable
+var menuStyle = document.querySelector("nav ul");
+
+//Set window resize to toggle function
+window.onresize = toggle;
+
+//Toggle function
+    //When the width of the screen is more than 751 - the media breakpoint - remove the responsive-style class
+    //This will force the responsive nav to always start from it's orginal state
+function toggle() {
+    if (window.innerWidth < 751) {
+        menuStyle.removeAttribute("class", "responsive-style");       
+    }
+}
+
+//Show nav function
+    //If the nav button is clicked, add the responsive-style class
+    //If the button is clicked again, remove the class
 function showNav() {
-    alert("hi!");
+    
+    if (this.wasClicked == false) {
+        menuStyle.setAttribute("class", "responsive-style");
+        this.wasClicked = true;
+    } else if (this.wasClicked == true) {
+        menuStyle.removeAttribute("class", "responsive-style");
+        this.wasClicked = false;
+    } 
 }
